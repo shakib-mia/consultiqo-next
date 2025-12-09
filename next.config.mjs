2ebf09a9d2af2bb9f21780b79e4671361ca80a7a
shakib-mia <smdshakibmia2001@gmail.com>
2025-12-09 10:30:05 +0600

    remote pattern for images fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "i.ibb.co",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "randomuser.me",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    // ],
-    domains: ["i.ibb.co", "randomuser.me"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    // remotePatterns: ["i.ibb.co", "randomuser.me"],
   },
 };
 
